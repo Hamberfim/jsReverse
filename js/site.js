ca4238b83a468619fca5f6d5cc2e63dda86c57cd
Anthony <adhamlingithub@gmail.com>
2021-07-29 14:33:07 -0500

    outline needed js

--- js/site.js
+++ js/site.js
@@ -1,2 +1,19 @@
 "use strict";
 
+// Controller function
+// get the input string value
+function getValue() {
+
+}
+
+// logic function
+// reverse the string value
+function reverseString() {
+
+}
+
+// view function
+// display the reversed string value back to the page
+function displayString() {
+
+}
