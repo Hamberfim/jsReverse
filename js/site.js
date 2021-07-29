ade74eed54c43ac11352fa145d5a57c349b4e309
Anthony <adhamlingithub@gmail.com>
2021-07-29 15:01:18 -0500

    js completed

--- js/site.js
+++ js/site.js
@@ -4,16 +4,41 @@
 // get the input string value
 function getValue() {
 
+    // make sure alert msg is not initially visible
+    document.getElementById('alert').classList.add('invisible');
+
+    let userStr = document.getElementById('userString').value;
+
+    let reverseStr = reverseString(userStr);
+
+    displayString(reverseStr);
+
 }
 
 // logic function
 // reverse the string value
-function reverseString() {
+function reverseString(userStr) {
+
+    let reverseStr = [];
+
+    // reverse string with a decrementing for loop
+    for (let i = userStr.length - 1; i >= 0; i--) {
+        reverseStr += userStr[i];
+        
+    }
+    return reverseStr;
 
 }
 
 // view function
 // display the reversed string value back to the page
-function displayString() {
+function displayString(reverseStr) {
+
+    // write to the page using a template literal
+    document.getElementById('msg').innerHTML = `${reverseStr}`;
+
+    // turn on inline alert success message
+    document.getElementById('alert').classList.remove('invisible');
+
 
 }
