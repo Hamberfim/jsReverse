42b7c7f49c526618605013fd45c335d98caf0544
Anthony <adhamlingithub@gmail.com>
2021-07-29 15:09:33 -0500

    more js comments, code page updates

--- js/site.js
+++ js/site.js
@@ -7,10 +7,13 @@ function getValue() {
     // make sure alert msg is not initially visible
     document.getElementById('alert').classList.add('invisible');
 
+    // get the user input string
     let userStr = document.getElementById('userString').value;
 
+    // call the reverse function
     let reverseStr = reverseString(userStr);
 
+    // pass the reversed string to the display function
     displayString(reverseStr);
 
 }
@@ -19,13 +22,17 @@ function getValue() {
 // reverse the string value
 function reverseString(userStr) {
 
+    // declare empty array to be populated
     let reverseStr = [];
 
     // reverse string with a decrementing for loop
     for (let i = userStr.length - 1; i >= 0; i--) {
+        // concatenate and populate the array with index values
         reverseStr += userStr[i];
         
     }
+
+    // retrun the reversed string
     return reverseStr;
 
 }
@@ -38,7 +45,7 @@ function displayString(reverseStr) {
     document.getElementById('msg').innerHTML = `${reverseStr}`;
 
     // turn on inline alert success message
+    // (remove 'invisible' from the class listings) 
     document.getElementById('alert').classList.remove('invisible');
-
 
 }
